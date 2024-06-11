36c263fb9a22e1cdcb33565d1c3909d9757c39ed
s-hirata0831 <e9248@g.maizuru-ct.ac.jp>
2024-06-11 14:37:13 +0900

    feat: ブラウザ機能を実装

--- arApp/ME-Bike/ME-Bike/ViewController.swift
+++ arApp/ME-Bike/ME-Bike/ViewController.swift
@@ -1,5 +1,6 @@
 import UIKit
 import ARKit
+import WebKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet var sceneView: ARSCNView!
@@ -33,5 +34,21 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
         // ARセッションを一時停止
         sceneView.session.pause()
+    }
+}
+
+class webViewController: UIViewController, WKNavigationDelegate{
+    @IBOutlet weak var webView: WKWebView!
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        webView.navigationDelegate = self
+        loadWebPage()
+    }
+    
+    func loadWebPage(){
+        if let url = URL(string: "https://karayab2024.web.app"){
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
     }
 }
