f44ae984076282fd62f6233028c5e479b56eff8a
s-hirata0831 <e9248@g.maizuru-ct.ac.jp>
2024-07-04 18:11:07 +0900

    test: RaspberryPi上のOpenCVが正しく動作するかテストするプログラムを追加

---
+++ spotApp/camTest.py
@@ -0,0 +1,22 @@
+import cv2
+
+# ウェブカメラのキャプチャを開始
+cap = cv2.VideoCapture(2)
+
+# キャプチャがオープンしている間続ける
+while(cap.isOpened()):
+    # フレームを読み込む
+    ret, frame = cap.read()
+    if ret == True:
+        # フレームを表示
+        cv2.imshow('Webcam Live', frame)
+
+        # 'q'キーが押されたらループから抜ける
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+
+# キャプチャをリリースし、ウィンドウを閉じる
+cap.release()
+cv2.destroyAllWindows()
