76ac8478b720dacfe8c9acf81a59afc8d3c8dd48
s-hirata0831 <e9248@g.maizuru-ct.ac.jp>
2024-07-06 16:46:33 +0900

    fix: 画面サイズを変更しました

--- spotApp/spotApp.py
+++ spotApp/spotApp.py
@@ -4,7 +4,7 @@ import flet as ft
 def main(page: ft.Page):
     page.title = "ME-Bike GUI"
     page.window_width = 1024
-    page.window_height = 600
+    page.window_height = 518
     page.window_minimizable = False
     page.window_maximizable = False
     page.window_resizable = False
