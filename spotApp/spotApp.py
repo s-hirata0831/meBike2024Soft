abf295915cf94e6e83f3f99130beeeaf9c1bd55b
s-hriata0831 <e9248@g.maizuru-ct.ac.jp>
2024-07-08 00:42:41 +0900

    feat: cahnge for raspberrypi

--- spotApp/spotApp.py
+++ spotApp/spotApp.py
@@ -4,25 +4,25 @@ import flet as ft
 def main(page: ft.Page):
     page.title = "ME-Bike GUI"
     page.window_width = 1024
-    page.window_height = 518
+    page.window_height = 600
     page.window_minimizable = False
     page.window_maximizable = False
     page.window_resizable = False
-    #page.window_top = 0  # 位置(TOP)
-    #page.window_left = 0  # 位置(LEFT)
+    page.window_full_screen = True
+    page.window_top = 1000  # 位置(TOP)
+    page.window_left = 10000  # 位置(LEFT)
     page.window_always_on_top = True
-    page.window_skip_task_bar = True #本番環境ではTrueにする
+    page.window_skip_task_bar = False #本番環境ではTrueにする
 
     text = ft.Text(
-        "ME-Bike GUIへようこそ！",
+        "Welcome to ME-Bike GUI",
         size=50,
         weight=ft.FontWeight.W_900,
         color=ft.colors.BLACK,
         selectable=False
-    )   
-
-    page.update()
+    )
     page.add(text)
+    page.update()
 
 
 ft.app(target=main)
