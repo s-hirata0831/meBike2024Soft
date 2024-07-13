be5a9d58abdd798a8017800e36049ec8fd1bb833
s-hirata0831 <e9248@g.maizuru-ct.ac.jp>
2024-07-14 00:04:46 +0900

    style: 余白を調節

--- spotApp/spotApp.py
+++ spotApp/spotApp.py
@@ -153,54 +153,44 @@ def main(page: ft.Page):
                             )],
                             alignment=ft.MainAxisAlignment.END
                         ),
-                        ft.Row(
-                            [ft.Text(
-                                "トークンを入力",
-                                size=60,
-                                weight=ft.FontWeight.W_900,
-                                color=ft.colors.BLACK,
-                                selectable=False,
-                                font_family="BIZ UDPGothic"
-                            )],
-                            alignment=ft.MainAxisAlignment.START
-                        ),
-                        ft.Row(
-                            [ft.Text(
-                                "Webアプリ上に以下の数字を入力してください。",
-                                size=50,
-                                weight=ft.FontWeight.W_900,
-                                color=ft.colors.BLACK,
-                                selectable=False,
-                                font_family="BIZ UDPGothic"
-                            )],
-                            alignment=ft.MainAxisAlignment.START
-                        ),
-                        ft.Row(
-                            [ft.Text(
-                                "入力できたら次へ進む。",
-                                size=50,
-                                weight=ft.FontWeight.W_900,
-                                color=ft.colors.BLACK,
-                                selectable=False,
-                                font_family="BIZ UDPGothic"
-                            )],
-                            alignment=ft.MainAxisAlignment.START
-                        ),
-                        ft.Row(
-                            [ft.Text(
-                                random,
-                                size=120,
-                                weight=ft.FontWeight.W_900,
-                                color=ft.colors.BLACK,
-                                selectable=False,
-                                font_family="BIZ UDPGothic",
-                                width=2000,
-                                height=1000,
-                                text_align=ft.TextAlign.CENTER
-                            )],
-                            alignment=ft.MainAxisAlignment.CENTER,
-                            vertical_alignment=ft.CrossAxisAlignment.CENTER
-                        )
+                        ft.Column([
+                            ft.Row([
+                                ft.Text(
+                                    "トークンを入力",
+                                    size=60,
+                                    weight=ft.FontWeight.W_900,
+                                    color=ft.colors.BLACK,
+                                    selectable=False,
+                                    font_family="BIZ UDPGothic"
+                                ),
+                                ft.Text(
+                                    "Webアプリ上に以下の数字を入力してください。",
+                                    size=50,
+                                    weight=ft.FontWeight.W_900,
+                                    color=ft.colors.BLACK,
+                                    selectable=False,
+                                    font_family="BIZ UDPGothic"
+                                ),
+                                ft.Text(
+                                    "入力できたら次へ進む",
+                                    size=50,
+                                    weight=ft.FontWeight.W_900,
+                                    color=ft.colors.BLACK,
+                                    selectable=False,
+                                    font_family="BIZ UDPGothic"
+                                )
+                            ],alignment=ft.MainAxisAlignment.START),
+                            ft.Row([
+                                ft.Text(
+                                    random,
+                                    size=120,
+                                    weight=ft.FontWeight.W_900,
+                                    color=ft.colors.BLACK,
+                                    selectable=False,
+                                    font_family="BIZ UDPGothic"
+                                )
+                            ],alignment=ft.MainAxisAlignment.CENTER)
+                        ],alignment=ft.MainAxisAlignment.SPACE_BETWEEN)
                     ]
                 )
             )
